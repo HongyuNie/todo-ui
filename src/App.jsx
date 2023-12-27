24ae7142a5b66ebafb600640e3f67626298fbaaf
hongyu <nhy0613@outlook.com>
2023-12-27 11:51:45 -0800

    configure routing for ListTodoComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ListTodoComponent from './components/ListTodoComponent'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
       <HeaderComponent/>
-      <ListTodoComponent/>
+      <Routes>
+        <Route path='/' element={<ListTodoComponent/>}></Route>
+        <Route path='/todos' element={<ListTodoComponent/>}></Route>
+      </Routes>
       <FooterComponent/>
+    </BrowserRouter>
     </>
   )
 }
