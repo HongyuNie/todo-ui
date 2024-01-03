7fb4ef6a67ac87d0dd464b43bc4f73f5282481fd
hongyu <nhy0613@outlook.com>
2024-01-02 18:38:43 -0800

    create saveLoggedInUser(), isUserLoggedIn(), getLoggedInUser() in AuthService.js, change Register and Loggin Buttons based on is UserLoggedIn() is true or false, change the route of root url to login page, save login user name to sessionStorage once user logged in successfully, refresh page to show the correct display.

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { loginAPICall, storeToken } from '../services/AuthService';
+import { loginAPICall, saveLoggedInUser, storeToken } from '../services/AuthService';
 import { useNavigate } from 'react-router-dom';
 
 const LoginComponent = () => {
@@ -9,17 +9,21 @@ const LoginComponent = () => {
 
   const navigator = useNavigate();
 
-  function handleLoginForm(e){
+  async function handleLoginForm(e){
     e.preventDefault();
     const loginObj = {username, password}
     // console.log(loginObj)
-    loginAPICall(username, password).then(response=>
+    await loginAPICall(username, password).then(response=>
       {
         console.log(response.data);
         const token = 'Basic ' + window.btoa(username + ":" + password);
         storeToken(token);
 
+        saveLoggedInUser(username);
+
         navigator("/todos");
+
+        window.location.reload(false);
       }).catch(e=>console.error(e))
 }
 
